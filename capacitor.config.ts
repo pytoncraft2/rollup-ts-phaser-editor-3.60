a6ddcbf367aa231d369d4590bad081f5cd204aaf
pytoncraft2 <pythoncraft2@gmail.com>
2023-04-18 01:11:37 +0200

    capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.huipat.aventure',
+  appName: 'huipat-aventure',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
